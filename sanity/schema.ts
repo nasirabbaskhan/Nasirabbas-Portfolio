e05e8d57f704b3dbf644116d29bf552c047341ae
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-04-27 03:31:30 -0700

    error resolved

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,4 @@
 import { type SchemaTypeDefinition } from "sanity";
-import hero from "./Schemas/hero";
 import about from "./Schemas/about";
 import education from "./Schemas/education";
 import skills from "./Schemas/skils";
@@ -7,5 +6,5 @@ import projects from "./Schemas/projects";
 import testimonial from "./Schemas/testimonial";
 import blog from "./Schemas/blog";
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [hero, about, education, skills, projects, testimonial, blog],
+  types: [about, education, skills, projects, testimonial, blog],
 };
